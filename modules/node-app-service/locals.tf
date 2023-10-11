e9aee491f946a0efc9a92e8eeb8740643561c894
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2023-10-11 14:00:06 +0100

    Merge https://github.com/Planning-Inspectorate/infrastructure-modules into feature/dev-69-add-docker-registry-password-to-kv

--- modules/node-app-service/locals.tf
+++ modules/node-app-service/locals.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 locals {
   app_settings = merge(
     var.app_settings,
@@ -25,26 +24,3 @@ locals {
 
   tags = var.tags
 }
-=======
-locals {
-  app_settings = merge(
-    var.app_settings,
-    {
-      DOCKER_ENABLE_CI                = true
-      DOCKER_REGISTRY_SERVER_PASSWORD = sensitive(local.secret_refs["docker-registry-server-password"])
-      DOCKER_REGISTRY_SERVER_URL      = data.azurerm_container_registry.acr.login_server
-      DOCKER_REGISTRY_SERVER_USERNAME = data.azurerm_container_registry.acr.admin_username
-    }
-  )
-
-  secrets_automated = [
-    "docker-registry-server-password"
-  ]
-
-  secret_names = concat(local.secrets_manual, local.secrets_automated)
-
-  secret_refs = {
-    for name in local.secret_names : name => "@Microsoft.KeyVault(SecretUri=${var.key_vault_uri}secrets/${name}/)"
-  }
-}
->>>>>>> 437b4983e0a86612d276558df88ec052980a0306
