2bded4e0e2975f8b2ebec83d046516cd70da5ff2
Hadrian Hughes <hadrian.hughes@kinandcarta.com>
2023-11-29 13:32:35 +0000

    Add optional health check path parameter to node-app-service

--- modules/node-app-service/main.tf
+++ modules/node-app-service/main.tf
@@ -31,8 +31,9 @@ resource "azurerm_linux_web_app" "web_app" {
   }
 
   site_config {
-    always_on     = true
-    http2_enabled = true
+    always_on         = true
+    http2_enabled     = true
+    health_check_path = var.health_check_path
 
     application_stack {
       docker_registry_url = "https://${data.azurerm_container_registry.acr.login_server}"
