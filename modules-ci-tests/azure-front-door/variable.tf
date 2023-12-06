fcf7f044f7037ca3521e52469c92b0f38c33a1ce
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2023-12-06 11:47:08 +0000

    Moved the location of ci tests for modules, updated directory name and updated module sources

---
+++ modules-ci-tests/azure-front-door/variable.tf
@@ -0,0 +1,33 @@
+variable "cdn_frontdoor_origin_path" {
+  description = "A directory path on the Front Door Origin that can be used to retrieve content"
+  type        = string
+}
+
+variable "common_log_analytics_workspace_id" {
+  description = "The ID for the common Log Analytics Workspace"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "The common resource tags for the project"
+  type        = map(string)
+}
+
+variable "name" {
+  description = "The name of the resource"
+  type        = string
+}
+variable "host_name" {
+  description = "The host name of the resource"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "service_name" {
+  description = "The name of the service the Front Door belongs to"
+  type        = string
+}
