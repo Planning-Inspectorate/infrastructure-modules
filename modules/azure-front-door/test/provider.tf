283c9e2ee0d38d032128687e7c6f6374c7755a9c
nasir-rahman9 <nasir.rahman@planninginspectorate.gov.uk>
2023-12-11 10:29:30 +0000

    Update provider files with tooling provider

--- modules/azure-front-door/test/provider.tf
+++ modules/azure-front-door/test/provider.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source                = "hashicorp/azurerm"
+      version               = "~> 3.0"
+      configuration_aliases = [azurerm, azurerm.tooling]
     }
   }
 
@@ -10,5 +11,12 @@ terraform {
 }
 
 provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "tooling"
+  subscription_id = var.tooling_subscription_id
+
   features {}
 }
