734996751b8250fac94fd7ad2a1aa5a0a77c7f84
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-01-16 10:57:05 +0000

    feat: run function apps from package by default

--- modules/node-function-app/locals.tf
+++ modules/node-function-app/locals.tf
@@ -1,5 +1,8 @@
 locals {
   app_settings = {
     SCM_DO_BUILD_DURING_DEPLOYMENT = false
+    # run from the zip file
+    # see https://learn.microsoft.com/en-gb/azure/azure-functions/run-functions-from-deployment-package#
+    WEBSITE_RUN_FROM_PACKAGE = 1
   }
 }
