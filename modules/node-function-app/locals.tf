616a8d2a81fb8f585ea0e467653ce8a08b91dc7d
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2024-05-13 16:29:23 +0100

    feat: remove app insights instance from function module

--- modules/node-function-app/locals.tf
+++ modules/node-function-app/locals.tf
@@ -5,13 +5,4 @@ locals {
     # see https://learn.microsoft.com/en-gb/azure/azure-functions/run-functions-from-deployment-package#
     WEBSITE_RUN_FROM_PACKAGE = 1
   }
-
-  app_insights = {
-    create = var.app_insights_instrument_key == null && var.use_app_insights
-    use    = var.app_insights_instrument_key != null
-  }
-  # either use the provided key, the created key, or none
-  app_insights_instrument_key = local.app_insights.use ? var.app_insights_instrument_key : (
-    local.app_insights.create ? azurerm_application_insights.function_app_insights[0].instrumentation_key : null
-  )
 }
