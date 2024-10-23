3d8c92bff95f45ca58134e26e3b72cf0109aeb77
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-10-23 11:53:39 +0100

    swithing sensitive to base64 on docker password

--- modules/node-app-service/locals.tf
+++ modules/node-app-service/locals.tf
@@ -3,7 +3,7 @@ locals {
     var.app_settings,
     {
       DOCKER_ENABLE_CI                = true
-      DOCKER_REGISTRY_SERVER_PASSWORD = sensitive(data.azurerm_container_registry.acr.admin_password)
+      DOCKER_REGISTRY_SERVER_PASSWORD = base64encode(data.azurerm_container_registry.acr.admin_password)
       DOCKER_REGISTRY_SERVER_URL      = data.azurerm_container_registry.acr.login_server
       DOCKER_REGISTRY_SERVER_USERNAME = data.azurerm_container_registry.acr.admin_username
     }
