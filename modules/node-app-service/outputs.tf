fdbf8f4f74921020c819dfc117b04988afe4f12e
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-10-23 13:22:33 +0100

    changing output to test access for container registry

--- modules/node-app-service/outputs.tf
+++ modules/node-app-service/outputs.tf
@@ -12,3 +12,8 @@ output "id" {
   description = "The ID of the App Service"
   value       = azurerm_linux_web_app.web_app.id
 }
+
+# testing for azurerm v4 upgrade
+output "acr_admin_username" {
+  value = data.azurerm_container_registry.acr.admin_username
+}
