f5518a8f9c5c2167bb3a32df852a123a452774d6
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-10-29 10:51:07 +0000

    providers file  node-function-app take out config alias for azurerm and azurerm.tooling

--- modules/node-app-service/test/provider.tf
+++ modules/node-app-service/test/provider.tf
@@ -1,19 +1,19 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source                = "hashicorp/azurerm"
-      version               = ">= 3.0, < 5.0"
-      configuration_aliases = [azurerm, azurerm.tooling]
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source                = "hashicorp/azurerm"
+#       version               = ">= 3.0, < 5.0"
+#       configuration_aliases = [azurerm, azurerm.tooling]
+#     }
+#   }
+# }
 
-provider "azurerm" {
-  alias           = "tooling"
-  subscription_id = var.tooling_subscription_id
-  features {}
-}
+# provider "azurerm" {
+#   alias           = "tooling"
+#   subscription_id = var.tooling_subscription_id
+#   features {}
+# }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
