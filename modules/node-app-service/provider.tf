2aebca8b045ec8f5e7f6c6cb3a8d34978ee4a358
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-10-30 08:49:22 +0000

    add in required_providers into node-app-service providers.tf

--- modules/node-app-service/provider.tf
+++ modules/node-app-service/provider.tf
@@ -1,12 +1,15 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source                = "hashicorp/azurerm"
-#       version               = ">= 3.0, < 5.0"
-#       configuration_aliases = [azurerm, azurerm.tooling]
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      version               = ">= 3.0, < 5.0"
+      configuration_aliases = [azurerm, azurerm.tooling]
+    }
+  }
+
+  required_version = ">= 1.5.7, < 1.10.0"
+}
+
 
 # provider "azurerm" {
 #   alias           = "tooling"
