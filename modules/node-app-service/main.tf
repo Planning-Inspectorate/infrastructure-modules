73b41fe14fefe2ce6e38a4f7d3e53ad54437f14b
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-02-26 10:55:58 +0000

    feat(node-app-service): add worker_count option

--- modules/node-app-service/main.tf
+++ modules/node-app-service/main.tf
@@ -45,6 +45,7 @@ resource "azurerm_linux_web_app" "web_app" {
     http2_enabled                     = true
     health_check_path                 = var.health_check_path
     health_check_eviction_time_in_min = var.health_check_eviction_time_in_min
+    worker_count                      = var.worker_count
 
     application_stack {
       docker_image_name        = "${var.image_name}:main"
