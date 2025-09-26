58f139a625e16dd9910fe621559d127762b9cb97
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-09-26 10:39:35 +0100

    fix(node-function-app): override function logging settings to remove excessive logging

--- modules/node-function-app/locals.tf
+++ modules/node-function-app/locals.tf
@@ -1,5 +1,11 @@
 locals {
   app_settings = {
+    # override host.json file log level
+    # see https://learn.microsoft.com/en-us/azure/azure-functions/configure-monitoring?tabs=v2#configure-log-levels
+    # and https://learn.microsoft.com/en-us/azure/azure-functions/functions-host-json#override-hostjson-values
+    AzureFunctionsJobHost__logging__fileLoggingMode   = "debugOnly"
+    AzureFunctionsJobHost__logging__logLevel__default = "Warning"
+
     SCM_DO_BUILD_DURING_DEPLOYMENT = false
     # run from the zip file
     # see https://learn.microsoft.com/en-gb/azure/azure-functions/run-functions-from-deployment-package#
