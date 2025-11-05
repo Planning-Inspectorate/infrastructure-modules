ca7cb4b0e207afb2d983c307be8b88ddfe99d332
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2025-11-05 13:42:21 +0000

    fix(infrastructure): adding tags module for testing

---
+++ modules/resource-tags/crown-tags.tf
@@ -0,0 +1,14 @@
+variable "base_tags" {
+  type        = map(string)
+  description = "Baseline tags defined at the project level"
+}
+
+variable "extra_tags" {
+  type        = map(string)
+  description = "Resource-specific tags to merge with baseline"
+  default     = {}
+}
+
+output "tags" {
+  value = merge(var.base_tags, var.extra_tags)
+}
