f6a03a0c9a8202d6d774d5df11aaaf9c0400b65f
Michael Juckes <michael.juckes@planninginspectorate.gov.uk>
2025-11-05 15:44:37 +0000

    fix(infrastructure): remove lifecycle tag on module

--- modules/resource-tags/crown-tags.tf
+++ modules/resource-tags/crown-tags.tf
@@ -1,15 +1,15 @@
-variable "base_tags" {
-  type        = map(string)
-  description = "Baseline tags defined at the project level"
-}
-
-variable "extra_tags" {
-  type        = map(string)
-  description = "Resource-specific tags to merge with baseline"
-  default     = {}
-}
-
-output "tags" {
-  description = "output for the tags"
-  value       = merge(var.base_tags, var.extra_tags)
-}
+# variable "base_tags" {
+#   type        = map(string)
+#   description = "Baseline tags defined at the project level"
+# }
+#
+# variable "extra_tags" {
+#   type        = map(string)
+#   description = "Resource-specific tags to merge with baseline"
+#   default     = {}
+# }
+#
+# output "tags" {
+#   description = "output for the tags"
+#   value       = merge(var.base_tags, var.extra_tags)
+# }
